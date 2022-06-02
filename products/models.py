e7ed17c4aafd45aa3dcffc936cdd9195bf0a9146
Bodin <bodintiago@gmail.com>
2022-06-02 20:35:11 +0200

    add product domain

--- products/models.py
+++ products/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 
+# Add validator for numerical fields
 class Product(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_length=16)
+    price = models.DecimalField
+    available_number = models.IntegerField
+    date_created = models.DateTimeField
+    date_updated = models.DateTimeField(default=date_created)
 
     def __str__(self):
         return f'{self.name}'
