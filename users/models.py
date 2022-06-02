c3c41432686576c484ab5ab76edbbd30741dbee8
Bodin <bodintiago@gmail.com>
2022-06-03 01:35:06 +0200

    switching to django built-in custom user model

--- users/models.py
+++ users/models.py
@@ -1,8 +1,5 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class User(models.Model):
-    username = models.CharField(max_length=64)
-    password = models.CharField(max_length=256)
-    email = models.EmailField
-    is_team = models.BooleanField(default=False)
+class User(AbstractUser):
+    pass
