cb6dfba15e5dd432ecbcd9fb66a4d73eaa5c64a0
Bodin <bodintiago@gmail.com>
2022-06-04 00:52:40 +0200

    assert content in responses body

--- users/tests.py
+++ users/tests.py
@@ -13,35 +13,49 @@ class UserModelTest(TestCase):
 
 """Functional tests"""
 
-c = Client()
-
 
 class UserManagementTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
     def test_register_invalid(self):
-        response = c.post('users/register', {'username': 'john', 'password': 'smith'})
-        self.assertEqual(response.status_code, 406)
+        response = self.client.post('/users/register',
+                                    {'username': 'john', "email": 'johnsmith@test.com',
+                                     'password': 'smith', "confirmation": 'sm'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Password must match confirmation")
 
     def test_register_valid(self):
-        response = c.post('users/register',
-                          {'username': 'john', "email": 'johnsmith@test.com', 'password': 'smith!990',
-                           "confirmation": 'smith!990'})
+        response = self.client.post('/users/register',
+                                    {'username': 'john', "email": 'johnsmith@test.com',
+                                     'password': 'smith!990', "confirmation": 'smith!990'})
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'User created successfully')
 
     def test_register_already_taken(self):
-        response = c.post('users/register',
-                          {'username': 'john', "email": 'john@test.com', 'password': 'smith!990',
-                           "confirmation": 'smith!990'})
-        self.assertEqual(response.status_code, 403)
+        self.client.post('/users/register',
+                         {'username': 'john', "email": 'john@test.com',
+                          'password': 'smith!990', "confirmation": 'smith!990'})
+        response = self.client.post('/users/register',
+                                    {'username': 'john', "email": 'john@test.com',
+                                     'password': 'smith!990', "confirmation": 'smith!990'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Username already taken")
 
 
 class UserAuthenticationTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
     def test_login_invalid(self):
-        response = c.post('users/login', {'username': 'abcd', 'password': 'fueizbjk-7428'})
-        self.assertEqual(response.status_code, 406)
+        response = self.client.post('/users/login', {'username': 'abcd', 'password': 'fueizbjk-7428'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Incorrect credentials')
 
     def test_login_valid(self):
-        response = c.post('users/login', {'username': 'john', 'password': 'smith!990'})
+        response = self.client.post('/users/login', {'username': 'john', 'password': 'smith!990'})
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Login successful')
 
     def test_logout(self):
         pass
