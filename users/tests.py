3c3ba5bc28e232faec44454a5a7d91a09673bfb0
Bodin <bodintiago@gmail.com>
2022-06-03 20:31:23 +0200

    replace assert by assertEqual

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase, Client
 from users.models import User
-from users.views import UserManagement, UserAuthentication
 
 
 """Unit tests"""
@@ -9,7 +8,7 @@ from users.views import UserManagement, UserAuthentication
 class UserModelTest(TestCase):
     def test_create(self):
         user = User.objects.create_user(username='georges', password='fnezin-13579')
-        assert user.username == 'georges'
+        self.assertEqual(user.username, 'georges')
 
 
 """Functional tests"""
@@ -20,29 +19,29 @@ c = Client()
 class UserManagementTest(TestCase):
     def test_register_invalid(self):
         response = c.post('users/register', {'username': 'john', 'password': 'smith'})
-        assert response.status_code == 406
+        self.assertEqual(response.status_code, 406)
 
     def test_register_valid(self):
         response = c.post('users/register',
                           {'username': 'john', "email": 'johnsmith@test.com', 'password': 'smith!990',
                            "confirmation": 'smith!990'})
-        assert response.status_code == 200
+        self.assertEqual(response.status_code, 200)
 
     def test_register_already_taken(self):
         response = c.post('users/register',
                           {'username': 'john', "email": 'john@test.com', 'password': 'smith!990',
                            "confirmation": 'smith!990'})
-        assert response.status_code == 403
+        self.assertEqual(response.status_code, 403)
 
 
 class UserAuthenticationTest(TestCase):
     def test_login_invalid(self):
         response = c.post('users/login', {'username': 'abcd', 'password': 'fueizbjk-7428'})
-        assert response.status_code == 406
+        self.assertEqual(response.status_code, 406)
 
     def test_login_valid(self):
         response = c.post('users/login', {'username': 'john', 'password': 'smith!990'})
-        assert response.status_code == 200
+        self.assertEqual(response.status_code, 200)
 
     def test_logout(self):
         pass
