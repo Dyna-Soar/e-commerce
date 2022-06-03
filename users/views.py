efb73b22a9a15d8c8d791d2905914cbe73f64a8f
Bodin <bodintiago@gmail.com>
2022-06-03 04:50:59 +0200

    handle errors, cleaner views, add docstrings

--- users/views.py
+++ users/views.py
@@ -1,31 +1,42 @@
 from django.contrib.auth import authenticate, login, logout
 
+from django.db import IntegrityError
+
 from users.models import User
 
 
 class UserManagement:
-    def create_user(self, request):
+    def register(self, request):
+        """Register new user"""
         if request.method == "POST":
-            data = request.POST
+            username = request.POST["username"]
+            email = request.POST["email"]
+
+            # Ensure password matches confirmation
+            password = request.POST["password"]
+            confirmation = request.POST["confirmation"]
+
+            if password != confirmation:
+                return "Password must match confirmation", 406
+
+            # Attempt to create new user
             try:
-                user = User.objects.create(
-                    username=data["username"],
-                    password=data["password"],
-                    email=data["email"]
-                )
+                user = User.objects.create_user(username, email, password)
                 user.save()
                 return 'User created successfully', 200
-            except AssertionError:
-                return 'Unable to create user', 406
+            except IntegrityError:
+                return "Username already taken", 403
         else:
             return 'Method not allowed', 405
 
 
 class UserAuthentication:
     def login_view(self, request):
+        """Login user"""
         if request.method == "POST":
-            data = request.POST
-            user = authenticate(request, username=data["username"], password=data["password"])
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
                 return 'Login successful', 200
@@ -33,6 +44,7 @@ class UserAuthentication:
                 return 'Incorrect credentials', 406
 
     def logout_view(self, request):
+        """Logout user"""
         logout(request)
-        return 'Login successful', 204
+        return 'Logout successful', 204
 
