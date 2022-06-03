1a29da6952742f786f4f347a430873939f18ffcc
Bodin <bodintiago@gmail.com>
2022-06-04 00:54:36 +0200

    update responses with the HttpResponse object

--- users/views.py
+++ users/views.py
@@ -1,50 +1,46 @@
 from django.contrib.auth import authenticate, login, logout
-
 from django.db import IntegrityError
-
+from django.http import HttpResponse
 from users.models import User
 
 
-class UserManagement:
-    def register(self, request):
-        """Register new user"""
-        if request.method == "POST":
-            username = request.POST["username"]
-            email = request.POST["email"]
+def register(request):
+    """Register new user"""
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
 
-            # Ensure password matches confirmation
-            password = request.POST["password"]
-            confirmation = request.POST["confirmation"]
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
 
-            if password != confirmation:
-                return "Password must match confirmation", 406
+        if password != confirmation:
+            return HttpResponse("Password must match confirmation")
 
-            # Attempt to create new user
-            try:
-                user = User.objects.create_user(username, email, password)
-                user.save()
-                return 'User created successfully', 200
-            except IntegrityError:
-                return "Username already taken", 403
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            return HttpResponse('User created successfully')
+        except IntegrityError:
+            return HttpResponse("Username already taken")
+    else:
+        return HttpResponse('Method not allowed')
+
+
+def login_view(request):
+    """Login user"""
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse('Login successful')
         else:
-            return 'Method not allowed', 405
+            return HttpResponse('Incorrect credentials')
 
 
-class UserAuthentication:
-    def login_view(self, request):
-        """Login user"""
-        if request.method == "POST":
-            username = request.POST["username"]
-            password = request.POST["password"]
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return 'Login successful', 200
-            else:
-                return 'Incorrect credentials', 406
-
-    def logout_view(self, request):
-        """Logout user"""
-        logout(request)
-        return 'Logout successful', 204
-
+def logout_view(request):
+    """Logout user"""
+    logout(request)
+    return HttpResponse('Logout successful')
