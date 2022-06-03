1f9645f12beb4cfa67d9b7a7c525e8adf4dba6a9
Bodin <bodintiago@gmail.com>
2022-06-03 12:38:49 +0200

    add tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from users.views import UserManagement, UserAuthentication
 
-# Create your tests here.
+
+c = Client()
+
+
+class UserManagementTest(TestCase):
+    def test_register_invalid(self):
+        response = c.post(UserManagement.register, {'username': 'john', 'password': 'smith'})
+        assert response.status_code == 406
+
+    def test_register_valid(self):
+        response = c.post(UserManagement.register,
+                          {'username': 'john', "email": 'johnsmith@test.com', 'password': 'smith!990',
+                           "confirmation": 'smith!990'})
+        assert response.status_code == 200
+
+    def test_register_already_taken(self):
+        response = c.post(UserManagement.register,
+                          {'username': 'john', "email": 'john@test.com', 'password': 'smith!990',
+                           "confirmation": 'smith!990'})
+        assert response.status_code == 403
+
+
+class UserAuthenticationTest(TestCase):
+    def test_login_invalid(self):
+        response = c.post(UserAuthentication.login_view, {'username': 'abcd', 'password': 'fueizbjk-7428'})
+        assert response.status_code == 406
+
+    def test_login_valid(self):
+        response = c.post(UserAuthentication.login_view, {'username': 'john', 'password': 'smith!990'})
+        assert response.status_code == 200
+
+    def test_logout(self):
+        pass
