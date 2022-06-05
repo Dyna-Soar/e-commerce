38aab0748148325d2e472b80334a3cbe22aa25e2
Bodin <bodintiago@gmail.com>
2022-06-05 16:07:51 +0200

    add clients.app

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    'clients.apps.ClientsConfig',
 ]
 
 MIDDLEWARE = [
