612d2195e2cfef7d4193848017da920333a2b20d
Bodin <bodintiago@gmail.com>
2022-06-05 17:43:31 +0200

    add unit tests for client and card creation

--- clients/tests.py
+++ clients/tests.py
@@ -10,15 +10,31 @@ class ClientModel(TestCase):
         client_user = ClientUser.objects.create(username='client_test1',
                                                 email='client@test.com',
                                                 password='fbjekzfje1654',
-                                                confirmation='fbjekzfje1654',
                                                 country='France',
                                                 zip_code='75010',
                                                 address='5 rue de paris')
 
+        self.assertEqual(client_user.username, 'client_test1')
+        self.assertEqual(client_user.zip_code, '75010')
+
 
 class CardModel(TestCase):
     def test_create_card(self):
-        card = Card.objects.create()
+        client_user = ClientUser.objects.create(username='client_test1',
+                                                email='client@test.com',
+                                                password='fbjekzfje1654',
+                                                country='France',
+                                                zip_code='75010',
+                                                address='5 rue de paris')
+
+        card = Card.objects.create(client=ClientUser.objects.get(username='client_test1'),
+                                   full_name='test_name test_surname',
+                                   card_network='Visa',
+                                   card_number=0,
+                                   card_security_code=123,
+                                   expiration_date='07/2028')
+        self.assertEqual(card.card_network, 'Visa')
+        self.assertEqual(card.client_id, 1)
 
 
 """Functional tests"""
