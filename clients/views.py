461b2fd1fb9dfefd7644ff9a70a747d10b46f54d
Bodin <bodintiago@gmail.com>
2022-06-05 21:22:38 +0200

    add register client view

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.db import IntegrityError
+from clients.models import ClientUser, Card
 
-# Create your views here.
+
+def client_register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        country = request.POST["country"]
+        zip_code = request.POST["zip_code"]
+        address = request.POST["address"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+
+        if password != confirmation:
+            return HttpResponse("Password must match confirmation")
+
+        # Attempt to create new user
+        try:
+            client_user = ClientUser.objects.create_user(username=username, email=email, password=password)
+            return HttpResponse('Client created successfully')
+        except IntegrityError:
+            return HttpResponse("Username already taken")
+    else:
+        return HttpResponse('Method not allowed')
