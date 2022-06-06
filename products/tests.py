09f7ebd1c29b5411d6046a9d6241af6b64e5c095
Bodin <bodintiago@gmail.com>
2022-06-06 18:31:51 +0200

    add view products test

--- products/tests.py
+++ products/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from django.urls import reverse
 from products.models import Product
 
 
@@ -24,3 +25,22 @@ class ProductManagement(TestCase):
                                     {'name': 'apple', 'description': '',
                                      'price': 0.5, 'number_in_store': 20})
         self.assertContains(response, 'Product has been created successfully')
+
+    def test_view_products(self):
+        response = self.client.post('/products/create_product',
+                                    {'name': 'apple', 'description': '',
+                                     'price': 0.5, 'number_in_store': 20})
+        response = self.client.get('/products/')
+        self.assertContains(response, 'apple')
+
+    def test_increase_number_item(self):
+        #product = Product.objects.get(id=1)
+        response = self.client.post(reverse('products:increase_number_item', args=[1]), {'increase_item': 2})
+        self.assertContains(response, 'Number of items has been increased successfully')
+        self.assertEqual(Product.objects.get(id=1).number_in_store, 22)
+
+    def decrease_number_item(self):
+        #product = Product.objects.get(id=1)
+        response = self.client.post(reverse('products/1/decrease_number_item', {'decrease_item': 4}))
+        self.assertContains(response, 'Number of items has been increased successfully')
+        self.assertEqual(Product.objects.get(id=1).number_in_store, 18)
