0d1121d6282a826ff1246384a8c8fce242f06807
Bodin <bodintiago@gmail.com>
2022-06-06 03:24:23 +0200

    add tests

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from products.models import Product
 
-# Create your tests here.
+
+"""Unit tests"""
+
+
+class ProductModel(TestCase):
+    def test_create_product(self):
+        new_product = Product.objects.create()
+        self.assertEqual(new_product.price, 1)
+
+
+"""Functional tests"""
+
+
+class ProductManagement(TestCase):
+    def set_up(self):
+        client = Client()
+
+    def test_create_product(self):
+        response = self.client.post('/products/create',
+                                    {'name': 'apple', 'description': '',
+                                     'price': 0.5, 'number_in_store': 20})
+        self.assertIn('Number of items has been increased successfully', response)
