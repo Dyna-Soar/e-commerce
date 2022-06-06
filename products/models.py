172f41a63611c653f33e88e90071dcd6b2ec6219
Bodin <bodintiago@gmail.com>
2022-06-06 03:06:46 +0200

    update datetime fields

--- products/models.py
+++ products/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
 
-# Add validator for numerical fields
 class Product(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField(blank=True)
-    price = models.DecimalField
-    available_number = models.IntegerField
-    date_created = models.DateTimeField
-    date_updated = models.DateTimeField(default=date_created)
+    price = models.DecimalField()
+    number_in_store = models.IntegerField()
+    date_created = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.name}'
--- products/views.py
+++ products/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from products.models import Product
 
-# Create your views here.
+
+def create_product(request):
+    if request.method == 'POST':
+        try:
+            new_product = Product.objects.create(name=request.POST['name'],
+                                                 description=request.POST['description'],
+                                                 price=request.POST['price'],
+                                                 number_in_store=request.POST['number_in_store'])
+            new_product.save()
+            return HttpResponse('Product has been created successfully')
+
+        except:
+            return HttpResponse('Error in creation')
+
+
+def update_product(request):
+    pass
+
+
+def increase_number_product(request, product_id, number_add):
+    if request.method == 'PUT':
+        product = None
+        try:
+            product = Product.objects.get(id=product_id)
+        except:
+            return HttpResponse('Error: could not find the product')
+
+        try:
+            product.number_in_store += number_add
+            product.save()
+            return HttpResponse('Number of items has been increased successfully')
+        except:
+            return HttpResponse('Error: could not increase the number of items')
+
+
+def decrease_number_product(request, product_id, number_sub):
+    if request.method == 'PUT':
+        product = None
+        try:
+            product = Product.objects.get(id=product_id)
+        except:
+            return HttpResponse('Error: could not find the product')
+
+        try:
+            product.number_in_store -= number_sub
+            product.save()
+            return HttpResponse('Number of items has been decreased successfully')
+        except:
+            return HttpResponse('Error: could not decrease the number of items')
