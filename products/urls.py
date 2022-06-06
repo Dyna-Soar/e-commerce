13b170c7a51cb63a52b22d4f224217ca41533e9d
Bodin <bodintiago@gmail.com>
2022-06-07 01:00:13 +0200

    delete urls that points to non-http operations

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,4 @@ app_name = 'products'
 urlpatterns = [
     path('', views.view_products, name='view_products'),
     path('create_product', views.create_product, name='create_product'),
-    path('<int:product_id>/increase_number_item', views.increase_number_item, name='increase_number_item'),
-    path('<int:product_id>/decrease_number_item', views.decrease_number_item, name='decrease_number_item'),
 ]
