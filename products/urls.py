237d344a84670775ad155f8d40ae0b55efd67459
Bodin <bodintiago@gmail.com>
2022-06-06 18:33:04 +0200

    view products url

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,8 @@ from products import views
 
 app_name = 'products'
 urlpatterns = [
+    path('', views.view_products, name='view_products'),
     path('create_product', views.create_product, name='create_product'),
-    path('increase_number_item/<int:product_id>/<int:add_item>', views.increase_number_item, name='increase_number_item'),
-    path('decrease_number_item/<int:product_id>/<int:sub_item>', views.decrease_number_item, name='decrease_number_item'),
+    path('<int:product_id>/increase_number_item', views.increase_number_item, name='increase_number_item'),
+    path('<int:product_id>/decrease_number_item', views.decrease_number_item, name='decrease_number_item'),
 ]
