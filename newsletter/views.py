e95541300e87905be3364e14b5589f9ad4621b39
Bodin <bodintiago@gmail.com>
2022-06-07 23:24:10 +0200

    enforce permission

--- newsletter/views.py
+++ newsletter/views.py
@@ -4,6 +4,8 @@ from newsletter.models import Subscriber
 
 
 def send_newsletter(request):
+    if request.user.is_staff is False:
+        return HttpResponse('You do not have permission to perform this action')
     if request.method == 'POST':
         emails_subscribers = Subscriber.objects.values_list('user__email')
         emails_subscribers_list = [x.email for x in emails_subscribers.iterator()]
