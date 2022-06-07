d1ae20d180332248a5f4dff5a9a5408bca7899fd
Bodin <bodintiago@gmail.com>
2022-06-07 20:02:47 +0200

    send newsletter view

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.core.mail import send_mass_mail
+from newsletter.models import Subscriber
 
-# Create your views here.
+
+def send_newsletter(request):
+    if request.method == 'POST':
+        emails_subscribers = Subscriber.objects.values_list('user__email')
+        emails_subscribers_list = [x for x in emails_subscribers.iterator()]
+        send_mass_mail(
+            request.POST['subject'],
+            request.POST['message'],
+            'no-reply@ecommerce.com',
+            emails_subscribers_list,
+        )
