2418c6a5fcc393bcbe0a519305b92ff5a3657b9d
Bodin <bodintiago@gmail.com>
2022-06-07 20:11:08 +0200

    debug emails list

--- newsletter/views.py
+++ newsletter/views.py
@@ -5,7 +5,7 @@ from newsletter.models import Subscriber
 def send_newsletter(request):
     if request.method == 'POST':
         emails_subscribers = Subscriber.objects.values_list('user__email')
-        emails_subscribers_list = [x for x in emails_subscribers.iterator()]
+        emails_subscribers_list = [x.email for x in emails_subscribers.iterator()]
         send_mass_mail(
             request.POST['subject'],
             request.POST['message'],
