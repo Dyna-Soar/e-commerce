30a19f4b199a7284f33566887bf52a1bbb62e4bd
Bodin <bodintiago@gmail.com>
2022-06-09 14:31:13 +0200

    update model's methods

--- orders/models.py
+++ orders/models.py
@@ -1,21 +1,24 @@
 from django.db import models
-
-from clients.models import Client
-
+from clients.models import ClientUser
 from products.models import Product
 
 
 class Order(models.Model):
     LEVEL_CHOICES = ["ACCEPTED", "IN_PREPARATION", "SHIPPED", "DELIVERED"]
 
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    client = models.ForeignKey(ClientUser, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     address_to_ship = models.CharField
     level = models.CharField(default="ACCEPTED")
     additional_information = models.TextField
 
     def ship(self):
-        self.level("SHIPPED").update()
+        self.level = "SHIPPED"
+        self.save()
 
     def deliver(self):
-        self.level("SHIPPED").update()
+        self.level = "DELIVERED"
+        self.save()
+
+    def __str__(self):
+        return f'{self.client.get_full_name}, shipment: {self.level}'
