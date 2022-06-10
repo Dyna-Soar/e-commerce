e7dfa2b6cbc0e7610ae3f9a6bb01b1c784058ed9
Bodin <bodintiago@gmail.com>
2022-06-10 14:58:03 +0200

    add OrderItem

--- orders/models.py
+++ orders/models.py
@@ -3,14 +3,20 @@ from clients.models import ClientUser
 from products.models import Product
 
 
+class OrderItem(models.Model):
+    client = models.ForeignKey(ClientUser, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL)
+    number_items = models.IntegerField()
+
+
 class Order(models.Model):
     LEVEL_CHOICES = ["ACCEPTED", "IN_PREPARATION", "SHIPPED", "DELIVERED"]
 
     client = models.ForeignKey(ClientUser, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product)
-    address_to_ship = models.CharField
+    products = models.ManyToManyField(OrderItem)
+    address_to_ship = models.CharField()
     level = models.CharField(default="ACCEPTED")
-    additional_information = models.TextField
+    additional_information = models.TextField(blank=True)
 
     def ship(self):
         self.level = "SHIPPED"
