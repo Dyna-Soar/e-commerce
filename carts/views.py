c652915f278021a627f3cdaee595fa5ea2b948f4
Bodin <bodintiago@gmail.com>
2022-06-16 18:14:49 +0200

    input session instead of request

--- carts/views.py
+++ carts/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render
 
 
-def create_cart(request):
-    if "cart" not in request.session:
-        request.session["cart"] = {"products": {}}
+def create_cart(user_session):
+    if "cart" not in user_session:
+        user_session["cart"] = {"products": {}}
 
 
-def add_product_to_cart(request, product, nb_items):
-    if product not in request.session["cart"]["products"]:
-        request.session["cart"]["products"][product] = {"nb_items": nb_items}
+def add_product_to_cart(user_session, product, nb_items):
+    if product not in user_session["cart"]["products"]:
+        user_session["cart"]["products"][product] = {"nb_items": nb_items}
 
 
-def add_nb_items_product(request, product, nb_items):
-    if product in request.session["cart"]["products"]:
-        request.session["cart"]["products"][product]["nb_items"] += nb_items
+def add_nb_items_product(user_session, product, nb_items):
+    if product in user_session["cart"]["products"]:
+        user_session["cart"]["products"][product]["nb_items"] += nb_items
 
 
-def remove_nb_items_product(request, product, nb_items):
-    if product in request.session["cart"]["products"]:
-        request.session["cart"]["products"][product]["nb_items"] -= nb_items
-        if request.session["cart"]["products"][product]["nb_items"] == 0:
-            request.session["cart"]["products"].remove(product)
+def remove_nb_items_product(user_session, product, nb_items):
+    if product in user_session["cart"]["products"]:
+        user_session["cart"]["products"][product]["nb_items"] -= nb_items
+        if user_session["cart"]["products"][product]["nb_items"] == 0:
+            user_session["cart"]["products"].remove(product)
